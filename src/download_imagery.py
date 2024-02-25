1d0c22539d440f1318e019e94a812a4fe5659e09
rybo-code <ryanbolger.nz@gmail.com>
2024-02-25 14:04:22 +0100

    Refactor, add logging, robustify functions

--- src/download_imagery.py
+++ src/download_imagery.py
@@ -1,12 +1,19 @@
 from pystac_client import Client
-from urllib.parse import urlparse
-from rioxarray import open_rasterio
-import geopandas as gpd
+import rioxarray
+import logging
+from tqdm import tqdm
+import os
+
+
+# Ignore lack of georeference for image thumbnails
+logging.basicConfig(level=logging.INFO)
 
 
 def search_elemnt84_stac(bbox, datetime, collections=["sentinel-2-l2a"]):
     """
     Identify the sentinel image file to download from AWS using the Element 84 STAC database
+
+    param :collections: List of satellite imagery STACs to look for. Use "sentinel-2-l2a" or "sentinel-s1-l1c"
     """
 
     client = Client.open("https://earth-search.aws.element84.com/v1")
@@ -17,48 +24,66 @@ def search_elemnt84_stac(bbox, datetime, collections=["sentinel-2-l2a"]):
     search = client.search(
         max_items=10, collections=collections, bbox=bbox, datetime=datetime
     )
-    print(f"{search.matched()} items found")
 
     item_collection = search.item_collection()
 
     return item_collection
 
 
-def download_from_aws_s3(item, asset_type):
+def download_from_aws_s3(item, asset_name, file_extension):
     """
     Save the image to a raster
-
-    param :item_collection: Xarray collection of returned features from client search
-    param :asset_types: See Element 84 docs for full list of images types eg. TCI (True Color Image)
-
-    return:
     """
     # TODO Add comments etc
 
-    image_href = item.assets[asset_type].href
-    image_obj = open_rasterio(image_href)
-    print(f"Filesize: {image_obj.nbytes/1024/1024:.2f}MB")
+    asset_name = asset_name.lower()
 
-    return image_obj
+    if file_extension == "jp2":
+        image_path_s3 = item.assets[asset_name + "-" + file_extension].href
+    else:
+        image_path_s3 = item.assets[asset_name].href
+    image_obj = rioxarray.open_rasterio(image_path_s3)
+    logging.info(f"Filesize: {image_obj.nbytes/1024/1024:.2f}MB")
+
+    # Get file extension from path eg .jpg, check it is correct
+    href_filetype = os.path.splitext(image_path_s3)[-1]
+    assert href_filetype == "." + file_extension
+
+    # Get Sentinel data structured name for image
+    file_name = item.id + href_filetype
+    file_path = f"./aws_data/{file_name}"
+
+    # Save images
+    image_obj.rio.to_raster(file_path)
+    logging.info(f"Saved image to {file_path}.")
+
+    return
 
 
 if __name__ == "__main__":
 
-    bbox = [32.68865076265763, 29.8744172395284, 32.47741813231619, 30.05545909539549]
-    datetime = ["2024-02-20", "2024-02-23"]
-    asset_types = ["thumbnail"]
-    stac_items_to_download = search_elemnt84_stac(bbox, datetime)
-    # TODO Create another script to locate desired files and save out as JSON
+    bbox = [
+        30.696510174036007,
+        46.265163784414824,
+        30.555737616687253,
+        46.35892215257451,
+    ]
 
-    for sentinel_capture in stac_items_to_download:
-        for asset_type in asset_types:
+    collections_to_search = ["sentinel-2-l2a"]
+    datetime = ["2024-02-20", "2024-02-21"]
+    asset_names = ["AOT"]
+    stac_items_to_download = search_elemnt84_stac(
+        bbox, datetime, collections=collections_to_search
+    )
+
+    logging.info(
+        f"{len(stac_items_to_download)} items found in {collections_to_search} collections for date/daterange {datetime}."
+    )
+    logging.info(f"Assets available: {stac_items_to_download[0].assets.keys()}")
+
+    # NOTE thumbnail.jpg and GeoTiff are available for open download, jpeg200 are not
+
+    for item in tqdm(stac_items_to_download[:1]):
+        for asset_name in asset_names:
             # Download imagery
-            image_obj = download_from_aws_s3(sentinel_capture, asset_types=[asset_type])
-
-            # Save images
-            filename = sentinel_capture.id + ".jpg"
-            data_path = f"./aws_data/{filename}"
-            image_obj.rio.to_raster(data_path + filename)
-            print(f"Saved image {filename}.")
-
-        # Images to be exported outside Docker container and stored as a GitHub Artifact
+            download_from_aws_s3(item, asset_name, file_extension="tif")
